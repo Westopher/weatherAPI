65b67d5e0bb6eead4280bee241e5c83d474f2f39
West Kraemer <kraemerwest@gmail.com>
2019-07-30 13:59:24 +0200

    add cllocation and delegates and location manager with prompts

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -1,8 +1,8 @@
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -11,7 +11,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -25,9 +25,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
-        
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     }
     
     
