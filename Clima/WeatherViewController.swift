1bcaf13e01af61d7ff627316e828bf4593d69c41
West Kraemer <kraemerwest@gmail.com>
2019-07-30 15:28:59 +0200

    construct lat and long w string interp in didupdatelocation function

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -71,9 +71,16 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Write the didUpdateLocations method here:
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        <#code#>
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            let params : [String : String] = ["lat": latitude, "long": longitude, "appid": APP_ID]
+        }
     }
-    
     
     //Write the didFailWithError method here:
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
